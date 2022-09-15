bb35d22e6dfa6f0252f32e429fc72877d2e169dc
HugoHenry701 <quanghieusieubeo@gmail.com>
2022-09-15 10:43:11 +0700

    test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Test script');
+  console.log('Test script force exit');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined.');
   }
