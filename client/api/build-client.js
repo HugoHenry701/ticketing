a32d5933bbf5a17a2de897329924edd6bcbe22b5
HugoHenry701 <quanghieusieubeo@gmail.com>
2022-09-15 16:48:19 +0700

    fix networking config digital ocean

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,7 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.htproduction.me/',
       headers: req.headers,
     });
   } else {
