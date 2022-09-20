ef1fba153bc2d2babcf926867f203ba8af937266
HugoHenry701 <quanghieusieubeo@gmail.com>
2022-09-20 19:52:52 +0700

    hot fix ingress-nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -15,7 +15,7 @@ const start = async () => {
   } catch (err) {
     console.log(err);
   }
-  const PORT = 3004;
+  const PORT = 3000;
   app.listen(PORT, () => {
     console.log(`Auth app is listening at port ${PORT}!!!!!!`);
   });
